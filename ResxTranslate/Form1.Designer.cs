dc902aba2ac7d220b32850b7ca371c84b4dda478
Maxim Nisenbaum <mnisenbaum@upstreamworks.com>
2016-12-16 21:41:16 +0000

    Create resource file translation project

---
+++ ResxTranslate/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ResxTranslate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadMainDictionaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadResourceFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.translateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Csv files|*.csv|Tab separated files|*.tsv|All files|*.*";
+            this.openFileDialog1.Title = "Open file";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 345);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(637, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(61, 17);
+            this.toolStripStatusLabel1.Text = "Dictionary";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadMainDictionaryToolStripMenuItem,
+            this.loadResourceFileToolStripMenuItem,
+            this.translateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(637, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadMainDictionaryToolStripMenuItem
+            // 
+            this.loadMainDictionaryToolStripMenuItem.Name = "loadMainDictionaryToolStripMenuItem";
+            this.loadMainDictionaryToolStripMenuItem.Size = new System.Drawing.Size(132, 20);
+            this.loadMainDictionaryToolStripMenuItem.Text = "Load Main Dictionary";
+            this.loadMainDictionaryToolStripMenuItem.Click += new System.EventHandler(this.loadMainDictionaryToolStripMenuItem_Click);
+            // 
+            // loadResourceFileToolStripMenuItem
+            // 
+            this.loadResourceFileToolStripMenuItem.Name = "loadResourceFileToolStripMenuItem";
+            this.loadResourceFileToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
+            this.loadResourceFileToolStripMenuItem.Text = "Load Resource File";
+            this.loadResourceFileToolStripMenuItem.Click += new System.EventHandler(this.loadResourceFileToolStripMenuItem_Click);
+            // 
+            // translateToolStripMenuItem
+            // 
+            this.translateToolStripMenuItem.Name = "translateToolStripMenuItem";
+            this.translateToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.translateToolStripMenuItem.Text = "Translate";
+            this.translateToolStripMenuItem.Click += new System.EventHandler(this.translateToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(637, 315);
+            this.textBox1.TabIndex = 2;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(55, 17);
+            this.toolStripStatusLabel2.Text = "Resource";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(20, 17);
+            this.toolStripStatusLabel3.Text = "->";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 367);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Resource Translate";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadMainDictionaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadResourceFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem translateToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+    }
+}
+
---
+++ ResxTranslate/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResxTranslate
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<String, String> _dictionary = new Dictionary<string, string>();
+        private Dictionary<String, String> _srcDictionary = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadMainDictionaryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "Tab separated files|*.tsv|Csv files|*.csv|All files|*.*";
+                var dlgres = openFileDialog1.ShowDialog();
+                if (dlgres == System.Windows.Forms.DialogResult.OK)
+                {
+                    toolStripStatusLabel1.Text = openFileDialog1.FileName;
+                    var loadtask = loadDictionary(openFileDialog1.FileName);
+                    loadtask.ContinueWith(x => {
+                        toolStripStatusLabel1.Text += string.Format(": {0}", x.Result.ToString());
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel4.Text = ex.Message;
+            }
+        }
+
+        private async Task<string> loadDictionary(string fileName)
+        {
+            // load dictionary file
+            using (var sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = await sr.ReadLineAsync();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        string[] args = line.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (args != null && args.Length > 1)
+                        {
+                            if (this._dictionary.Count == 0) // headers
+                            {
+                                textBox1.AppendText(String.Format("{0} -> {1}{2}", args[0], args[1], Environment.NewLine));
+                            }
+                            if (!this._dictionary.ContainsKey(args[0]))
+                            { // ignore duplicates
+                                this._dictionary.Add(args[0], args[1]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return this._dictionary.Count.ToString();
+        }
+
+        private void loadResourceFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "Resource files|*.resx|Csv files|*.csv|All files|*.*";
+                var dlgres = openFileDialog1.ShowDialog();
+                if (dlgres == System.Windows.Forms.DialogResult.OK)
+                {
+                    toolStripStatusLabel2.Text = openFileDialog1.FileName;
+                    // load en resource file
+                    using (ResXResourceReader resxReader = new ResXResourceReader(openFileDialog1.FileName))
+                    {
+                        foreach (DictionaryEntry entry in resxReader)
+                        {
+                            this._srcDictionary.Add(entry.Key.ToString(), entry.Value.ToString());
+                            textBox1.AppendText(String.Format("{0} -> {1}{2}", entry.Key, entry.Value, Environment.NewLine)); 
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel4.Text = ex.Message;
+            }
+        }
+
+        private void translateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = Path.Combine(Path.GetDirectoryName(openFileDialog1.FileName), 
+                    String.Format("{0}.es{1}", Path.GetFileNameWithoutExtension(openFileDialog1.FileName), Path.GetExtension(openFileDialog1.FileName)));
+                using (ResXResourceWriter resxWriter = new ResXResourceWriter(fileName))
+                {
+                    var q = from line in this._srcDictionary.AsQueryable()
+                            select line;
+
+                    foreach (var entry in q.ToList())
+                    {
+                        if (this._dictionary.ContainsKey(entry.Value.ToString()))
+                        {
+                            var translation = this._dictionary.FirstOrDefault(x => x.Key.ToString().Equals(entry.Value.ToString()));
+                            resxWriter.AddResource(entry.Key.ToString(), translation.Value.ToString());
+                        }
+                        else // translation not found
+                        {
+                            var node = new ResXDataNode(entry.Key.ToString(), entry.Value.ToString());
+                            node.Comment = "to be translated";
+                            resxWriter.AddResource(node);
+                        }
+                    }
+                }
+                toolStripStatusLabel4.Text = "Done";
+            }
+            catch (Exception ex)
+            {
+                toolStripStatusLabel4.Text = ex.Message;
+            }
+        }
+    }
+}
